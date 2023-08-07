76c24bafe74b5ab35ddeb51227b7085be4647b41
Dan Li <danmuxingli@gmail.com>
2023-08-06 22:02:24 -0400

    front-end search button sample query count handler

--- frontend-app/src/renderer/src/components/Safari.vue
+++ frontend-app/src/renderer/src/components/Safari.vue
@@ -1,15 +1,28 @@
+<script>
+import '../assets/css/search.css';
+import { ref } from 'vue';
+
+export default {
+  name: 'Safari',
+  setup() {
+    const counter = ref(0);
+    return {
+      counter
+    };
+  }
+}
+</script>
+
 <template>
   <div class="container">
     <div class="search-bar">
       <input type="text" class="search-input" placeholder="Enter your query">
-      <button class="search-button">&#x1F50E;&#xFE0E;</button>
+      <button class="search-button" @click="counter++">&#x1F50E;&#xFE0E;</button>
     </div>
   </div>
+  <div class="query-count">
+    <p>
+      <b>Number of Queries: {{ counter }}</b>
+    </p>
+  </div>
 </template>
-
-<script>
-import '../assets/css/search.css';
-export default {
-  name: 'Safari'
-}
-</script>
